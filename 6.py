59eab96bc2f2b8d5a4f87db7b74778706d56d509
antoprinciya <51980780+antoprinciya@users.noreply.github.com>
2019-07-27 13:43:06 +0530

    Delete 6.py

--- 6.py
+++
@@ -1,9 +0,0 @@
-sum=int(input())
-array=list(map(int,input().split()))
-an=0
-for p in range(len(array)-2):
-    for z in range(p+1,len(array)-1):
-        for r in range(z+1,len(array)):
-            if array[p]<array[z]<array[r] and p<z<r:
-                an=an+r
-print(an)
